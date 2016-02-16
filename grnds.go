870d1867654c4e91343c6824fdbf8481f47a8a20
Susumu Yata <susumu.yata@gmail.com>
2016-02-17 05:51:26 +0900

    Implement SelectWithSortby()
    GitHub: #1

--- grnds.go
+++ grnds.go
@@ -1,12 +1,12 @@
 package grnds
 
 import (
-//	"bytes"
+	//	"bytes"
 	"fmt"
 	"reflect"
 	"strings"
 	"sync"
-//	"time"
+	//	"time"
 
 	"github.com/groonga/grnci"
 )
@@ -133,14 +133,204 @@ func (db *DB) selectWithoutSortby(tbl string, vals interface{}, options *grnci.S
 	return outN, nil
 }
 
+func compareFields(lhsVal, rhsVal reflect.Value) int {
+	switch lhs := lhsVal.Interface().(type) {
+	case grnci.Bool:
+		rhs := rhsVal.Interface().(grnci.Bool)
+		if !lhs && rhs {
+			return -1
+		} else if lhs == rhs {
+			return 0
+		} else {
+			return 1
+		}
+	case grnci.Int:
+		rhs := rhsVal.Interface().(grnci.Int)
+		if lhs < rhs {
+			return -1
+		} else if lhs == rhs {
+			return 0
+		} else {
+			return 1
+		}
+	case grnci.Float:
+		rhs := rhsVal.Interface().(grnci.Float)
+		if lhs < rhs {
+			return -1
+		} else if lhs == rhs {
+			return 0
+		} else {
+			return 1
+		}
+	case grnci.Time:
+		rhs := rhsVal.Interface().(grnci.Time)
+		if lhs < rhs {
+			return -1
+		} else if lhs == rhs {
+			return 0
+		} else {
+			return 1
+		}
+	case grnci.Text:
+		rhs := rhsVal.Interface().(grnci.Text)
+		if lhs < rhs {
+			return -1
+		} else if lhs == rhs {
+			return 0
+		} else {
+			return 1
+		}
+	default:
+		return 0
+	}
+}
+
+type Cond struct {
+	Field *grnci.FieldInfo
+	Neg   bool
+}
+
+//type Resp struct {
+//	N    int
+//	Recs []Rec
+//}
+
+func isPrior(conds []Cond, lhsResp, rhsResp reflect.Value) bool {
+	lhsVal := lhsResp.Index(0)
+	rhsVal := rhsResp.Index(0)
+	for _, cond := range conds {
+		lhsField := lhsVal.Field(cond.Field.ID())
+		rhsField := rhsVal.Field(cond.Field.ID())
+		var res int
+		if !cond.Neg {
+			res = compareFields(lhsField, rhsField)
+		} else {
+			res = compareFields(rhsField, lhsField)
+		}
+		if res == 0 {
+			continue
+		}
+		return res < 0
+	}
+	return false
+}
+
+func chooseMostPrior(conds []Cond, resps []reflect.Value) int {
+	priorID := 0
+	for i := 1; i < len(resps); i++ {
+		if isPrior(conds, resps[i], resps[priorID]) {
+			priorID = i
+		}
+	}
+	return priorID
+}
+
 func (db *DB) selectWithSortby(tbl string, vals interface{}, options *grnci.SelectOptions, sortby []string) (int, error) {
-	// TODO: adjust options.
+	// Add --sortby to --output_columns if required.
+	outCols := splitValues(options.OutputColumns, ',')
+	if len(outCols) != 0 {
+		for _, sortby := range sortby {
+			if strings.HasPrefix(sortby, "-") {
+				sortby = sortby[1:]
+			}
+			included := false
+			for _, outCol := range outCols {
+				if sortby == outCol {
+					included = true
+					break
+				}
+			}
+			if !included {
+				outCols = append(outCols, sortby)
+			}
+		}
+	}
 
-	// TODO: execute `select` in parallel.
+	// Adjust --offset and --limit.
+	if options.Offset < 0 {
+		return 0, fmt.Errorf("invalid offset: offset = %d", options.Offset)
+	}
+	localOptions := *options
+	if options.Limit > 0 {
+		localOptions.Limit += options.Offset
+	}
+	localOptions.Offset = 0
 
-	// TODO: merge the responses.
+	// Execute `select` in parallel.
+	var wg sync.WaitGroup
+	wg.Add(len(db.dbs))
+	ns := make([]int, len(db.dbs))
+	resps, err := db.makeEmptyResponses(vals)
+	if err != nil {
+		return 0, err
+	}
+	errs := make([]error, len(db.dbs))
+	for i, _ := range db.dbs {
+		go func(i int) {
+			ns[i], errs[i] = db.dbs[i].Select(tbl, resps[i].Interface(), &localOptions)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	for _, err := range errs {
+		if err != nil {
+			return 0, err
+		}
+	}
 
-	return 0, nil
+	// Find fields used in sorting.
+	info := grnci.GetStructInfo(vals)
+	if err := info.Error(); err != nil {
+		return 0, err
+	}
+	conds := make([]Cond, len(sortby))
+	for i, entry := range sortby {
+		if conds[i].Neg = strings.HasPrefix(entry, "-"); conds[i].Neg {
+			entry = entry[1:]
+		}
+		if conds[i].Field = info.FieldByColumnName(entry); conds[i].Field == nil {
+			return 0, fmt.Errorf("no such field: entry = %s", entry)
+		}
+	}
+
+	// Merge the responses.
+	outN := 0
+	outRecs := reflect.ValueOf(vals).Elem()
+	nNonEmpties := 0
+	for i := 0; i < len(db.dbs); i++ {
+		outN += ns[i]
+		if resps[i].IsNil() {
+			continue
+		}
+		resps[i] = resps[i].Elem()
+		if resps[i].Len() == 0 {
+			continue
+		}
+		resps[nNonEmpties] = resps[i]
+		nNonEmpties++
+	}
+	resps = resps[:nNonEmpties]
+	for (len(resps) != 0) && (outRecs.Len() < (options.Offset + options.Limit)) {
+		respID := chooseMostPrior(conds, resps)
+		outRecs = reflect.Append(outRecs, resps[respID].Index(0))
+		resps[respID] = resps[respID].Slice(1, resps[respID].Len())
+		if resps[respID].Len() == 0 {
+			for i := respID + 1; i < len(resps); i++ {
+				resps[i-1] = resps[i]
+			}
+			resps = resps[:len(resps)-1]
+		}
+	}
+	if options.Offset < outRecs.Len() {
+		outRecs = outRecs.Slice(options.Offset, outRecs.Len())
+	} else {
+		outRecs = outRecs.Slice(outRecs.Len(), outRecs.Len())
+	}
+	if (options.Limit >= 0) && (options.Limit < outRecs.Len()) {
+		outRecs = outRecs.Slice(0, options.Limit)
+	}
+	reflect.ValueOf(vals).Elem().Set(outRecs)
+	return outN, nil
 }
 
 func (db *DB) Select(tbl string, vals interface{}, options *grnci.SelectOptions) (int, error) {
@@ -154,194 +344,3 @@ func (db *DB) Select(tbl string, vals interface{}, options *grnci.SelectOptions)
 		return db.selectWithSortby(tbl, vals, options, sortby)
 	}
 }
-
-//type Resp struct {
-//	N    int
-//	Recs []Rec
-//}
-
-//type Cond struct {
-//	Field *grnci.FieldInfo
-//	Neg   bool
-//}
-
-//func compareFields(lhsVal, rhsVal reflect.Value) int {
-//	switch lhs := lhsVal.Interface().(type) {
-//	case grnci.Bool:
-//		rhs := rhsVal.Interface().(grnci.Bool)
-//		if !lhs && rhs {
-//			return -1
-//		} else if lhs == rhs {
-//			return 0
-//		} else {
-//			return 1
-//		}
-//	case grnci.Int:
-//		rhs := rhsVal.Interface().(grnci.Int)
-//		if lhs < rhs {
-//			return -1
-//		} else if lhs == rhs {
-//			return 0
-//		} else {
-//			return 1
-//		}
-//	case grnci.Float:
-//		rhs := rhsVal.Interface().(grnci.Float)
-//		if lhs < rhs {
-//			return -1
-//		} else if lhs == rhs {
-//			return 0
-//		} else {
-//			return 1
-//		}
-//	case grnci.Time:
-//		rhs := rhsVal.Interface().(grnci.Time)
-//		if lhs < rhs {
-//			return -1
-//		} else if lhs == rhs {
-//			return 0
-//		} else {
-//			return 1
-//		}
-//	case grnci.Text:
-//		rhs := rhsVal.Interface().(grnci.Text)
-//		if lhs < rhs {
-//			return -1
-//		} else if lhs == rhs {
-//			return 0
-//		} else {
-//			return 1
-//		}
-//	default:
-//		log.Fatal("unsupported data type: ", lhsVal.Type())
-//	}
-//	return -1
-//}
-
-//func isPrior(conds []Cond, lhsResp, rhsResp Resp) bool {
-//	lhsVal := reflect.ValueOf(lhsResp.Recs[0])
-//	rhsVal := reflect.ValueOf(rhsResp.Recs[0])
-//	for _, cond := range conds {
-//		lhsField := lhsVal.Field(cond.Field.ID())
-//		rhsField := rhsVal.Field(cond.Field.ID())
-//		var res int
-//		if !cond.Neg {
-//			res = compareFields(lhsField, rhsField)
-//		} else {
-//			res = compareFields(rhsField, lhsField)
-//		}
-//		if res == 0 {
-//			continue
-//		}
-//		return res < 0
-//	}
-//	return false
-//}
-
-//func chooseMostPrior(conds []Cond, resps []Resp) int {
-//	priorID := 0
-//	for i := 1; i < len(resps); i++ {
-//		if isPrior(conds, resps[i], resps[priorID]) {
-//			priorID = i
-//		}
-//	}
-//	return priorID
-//}
-
-//func selectWithSortby(dbs []*grnci.DB, options grnci.SelectOptions, sortby []string) (outN int, outRecs []Rec) {
-//	// Add --sortby to --output_columns if required.
-//	output := splitValues(options.OutputColumns, ',')
-//	if len(output) != 0 {
-//		for _, sortby := range sortby {
-//			if strings.HasPrefix(sortby, "-") {
-//				sortby = sortby[1:]
-//			}
-//			included := false
-//			for _, output := range output {
-//				if sortby == output {
-//					included = true
-//					break
-//				}
-//			}
-//			if !included {
-//				output = append(output, sortby)
-//			}
-//		}
-//	}
-
-//	// Adjust --offset and --limit.
-//	offset := options.Offset
-//	limit := options.Limit
-//	if offset < 0 {
-//		log.Fatal("negative offset is not supported")
-//	}
-//	if limit > 0 {
-//		options.Limit += offset
-//	}
-//	options.Offset = 0
-
-//	// Execute `select` in parallel.
-//	var wg sync.WaitGroup
-//	wg.Add(len(dbs))
-//	resps := make([]Resp, len(dbs))
-//	for i, _ := range dbs {
-//		go func(i int) {
-//			var recs []Rec
-//			n, err := dbs[i].Select("Page", &recs, &options)
-//			if err != nil {
-//				log.Fatal("grnci.DB.Select() failed: ", err)
-//			}
-//			resps[i] = Resp{n, recs}
-//			wg.Done()
-//		}(i)
-//	}
-//	wg.Wait()
-
-//	// Find fields used in sorting.
-//	info := grnci.GetStructInfo(outRecs)
-//	if err := info.Error(); err != nil {
-//		log.Fatal("grnci.GetStructInfo() failed: ", err)
-//	}
-//	conds := make([]Cond, len(sortby))
-//	for i, sortby := range sortby {
-//		if conds[i].Neg = strings.HasPrefix(sortby, "-"); conds[i].Neg {
-//			sortby = sortby[1:]
-//		}
-//		if conds[i].Field = info.FieldByColumnName(sortby); conds[i].Field == nil {
-//			log.Fatal("grnci.StructInfo.FieldByColumnName() failed: ", sortby)
-//		}
-//	}
-
-//	// Merge the responses.
-//	cnt := 0
-//	for i := 0; i < len(resps); i++ {
-//		outN += resps[i].N
-//		if len(resps[i].Recs) == 0 {
-//			continue
-//		}
-//		resps[cnt] = resps[i]
-//		cnt++
-//	}
-//	resps = resps[:cnt]
-//	for (len(resps) != 0) && (len(outRecs) < (offset + limit)) {
-//		respID := chooseMostPrior(conds, resps)
-//		outRecs = append(outRecs, resps[respID].Recs[0])
-//		resps[respID].Recs = resps[respID].Recs[1:]
-//		if len(resps[respID].Recs) == 0 {
-//			for i := respID + 1; i < len(resps); i++ {
-//				resps[i-1] = resps[i]
-//			}
-//			resps = resps[:len(resps)-1]
-//		}
-//	}
-//	return
-//}
-
-//func runSelect(dbs []*grnci.DB, options *grnci.SelectOptions) (int, []Rec) {
-//	sortby := splitValues(options.Sortby, ',')
-//	if len(sortby) == 0 {
-//		return selectWithoutSortby(dbs, *options)
-//	} else {
-//		return selectWithSortby(dbs, *options, sortby)
-//	}
-//}
--- grnds_test.go
+++ grnds_test.go
@@ -2,6 +2,7 @@ package grnds
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"path/filepath"
 	"testing"
@@ -9,7 +10,7 @@ import (
 	"github.com/groonga/grnci"
 )
 
-func TestMain(t *testing.T) {
+func TestSelectWithoutSortby(t *testing.T) {
 	type Rec struct {
 		Val grnci.Int
 	}
@@ -27,8 +28,11 @@ func TestMain(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		rec := Rec{Val: grnci.Int(i * 100)}
-		if _, err := db.LoadEx("tbl", rec, nil); err != nil {
+		recs := make([]Rec, 5)
+		for j, _ := range recs {
+			recs[j].Val = grnci.Int(rand.Intn(100))
+		}
+		if _, err := db.LoadEx("tbl", recs, nil); err != nil {
 			t.Fatal(err)
 		}
 		db.Close()
@@ -41,11 +45,145 @@ func TestMain(t *testing.T) {
 			t.Fatal(err)
 		}
 	}
-	var recs []Rec
-	n, err := db.Select("tbl", &recs, nil)
-	if err != nil {
+
+	{
+		var recs []Rec
+		n, err := db.Select("tbl", &recs, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("default: n = %v, recs = %v\n", n, recs)
+	}
+
+	{
+		options := grnci.NewSelectOptions()
+		options.Limit = -1
+		var recs []Rec
+		n, err := db.Select("tbl", &recs, options)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("limit = %d: n = %v, recs = %v\n", options.Limit, n, recs)
+	}
+
+	{
+		options := grnci.NewSelectOptions()
+		options.Offset = 10
+		var recs []Rec
+		n, err := db.Select("tbl", &recs, options)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("offset = %d: n = %v, recs = %v\n", options.Offset, n, recs)
+	}
+
+	{
+		options := grnci.NewSelectOptions()
+		options.Filter = "Val<50"
+		var recs []Rec
+		n, err := db.Select("tbl", &recs, options)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("filter = \"%s\": n = %v, recs = %v\n", options.Filter, n, recs)
+	}
+}
+
+func TestSelectWithSortby(t *testing.T) {
+	type Rec struct {
+		Val grnci.Int
+	}
+
+	if err := os.RemoveAll("/tmp/grnds"); err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println("n:", n)
-	fmt.Println("recs:", recs)
+	for i := 0; i < 3; i++ {
+		dir := fmt.Sprintf("/tmp/grnds/%02d", i)
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			t.Fatal(err)
+		}
+		name := filepath.Join(dir, "db")
+		db, err := grnci.Create(name)
+		if err != nil {
+			t.Fatal(err)
+		}
+		recs := make([]Rec, 5)
+		for j, _ := range recs {
+			recs[j].Val = grnci.Int(rand.Intn(100))
+		}
+		if _, err := db.LoadEx("tbl", recs, nil); err != nil {
+			t.Fatal(err)
+		}
+		db.Close()
+	}
+
+	db := NewDB()
+	for i := 0; i < 3; i++ {
+		name := fmt.Sprintf("/tmp/grnds/%02d/db", i)
+		if err := db.Open(name); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	{
+		options := grnci.NewSelectOptions()
+		options.Sortby = "Val"
+		var recs []Rec
+		n, err := db.Select("tbl", &recs, options)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("sortby = \"%s\": n = %v, recs = %v\n", options.Sortby, n, recs)
+	}
+
+	{
+		options := grnci.NewSelectOptions()
+		options.Sortby = "-Val"
+		var recs []Rec
+		n, err := db.Select("tbl", &recs, options)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("sortby = \"%s\": n = %v, recs = %v\n", options.Sortby, n, recs)
+	}
+
+	{
+		options := grnci.NewSelectOptions()
+		options.Sortby = "Val"
+		options.Offset = 5
+		var recs []Rec
+		n, err := db.Select("tbl", &recs, options)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("sortby = \"%s\", offset = %d: n = %v, recs = %v\n",
+			options.Sortby, options.Offset, n, recs)
+	}
+
+	{
+		options := grnci.NewSelectOptions()
+		options.Sortby = "Val"
+		options.Offset = 3
+		options.Limit = 5
+		var recs []Rec
+		n, err := db.Select("tbl", &recs, options)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("sortby = \"%s\", offset = %d, limit = %d: n = %v, recs = %v\n",
+			options.Sortby, options.Offset, options.Limit, n, recs)
+	}
+
+	{
+		options := grnci.NewSelectOptions()
+		options.Filter = "Val%2==0"
+		options.Sortby = "Val"
+		var recs []Rec
+		n, err := db.Select("tbl", &recs, options)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("sortby = \"%s\", filter = \"%s\": n = %v, recs = %v\n",
+			options.Sortby, options.Filter, n, recs)
+	}
 }
